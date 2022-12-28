575bae0da11e71916c21c653cc4aea5d7a6240aa
Tino Tusaumwe <tusatino@gmail.com>
2022-12-28 11:58:23 +1000

    Add color extensions for asset colors

---
+++ IssueTracker/Extensions/Color+Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  Color+Extensions.swift
+//  IssueTracker
+//
+//  Created by Tino on 28/12/2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let background = Color("background")
+    static let buttonBackground = Color("buttonBackground")
+    static let buttonLabel = Color("buttonLabel")
+    static let buttonText = Color("buttonText")
+    static let customSecondary = Color("customSecondary")
+    static let deleteColour = Color("deleteColour")
+    static let popup = Color("popup")
+    static let tagSelected = Color("tagSelected")
+    static let tagUnselected = Color("tagUnselected")
+    static let text = Color("text")
+}
