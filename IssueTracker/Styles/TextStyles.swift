18bf9cc14e2618a5b441340bac0e3cee2805b32a
Tino Tusaumwe <tusatino@gmail.com>
2022-12-28 12:21:33 +1000

    Add app test styles

---
+++ IssueTracker/Styles/TextStyles.swift
@@ -0,0 +1,93 @@
+//
+//  TextStyles.swift
+//  IssueTracker
+//
+//  Created by Tino on 28/12/2022.
+//
+
+import SwiftUI
+
+struct TextStyles: View {
+    var body: some View {
+        VStack {
+            Text("Title style")
+                .titleStyle()
+            Text("Header")
+                .headerStyle()
+            Text("This is the body style.")
+                .bodyStyle()
+            Text("This is the footer style.")
+                .footerStyle()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.customBackground)
+
+    }
+}
+
+/// Text styles for the app.
+private struct TextStyle: ViewModifier {
+    /// The fonts in the app.
+    enum FontType {
+        case courierNew
+        
+        var fontName: String {
+            switch self {
+            case .courierNew: return "Courier New"
+            }
+        }
+    }
+    
+    private let size: Double
+    private let font: FontType?
+    private let textStyle: Font.TextStyle
+    
+    init(size: Double, font: FontType? = nil, relativeTo textStyle: Font.TextStyle) {
+        self.size = size
+        self.font = font
+        self.textStyle = textStyle
+    }
+    
+    func body(content: Content) -> some View {
+        if let font {
+            content
+                .font(.custom(font.fontName, size: size, relativeTo: textStyle))
+        } else {
+            content
+                .font(.system(size: size))
+        }
+    }
+}
+
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(TextStyle(size: 30, font: .courierNew, relativeTo: .title))
+            .fontWeight(.light)
+            .foregroundColor(.text)
+    }
+    
+    func headerStyle() -> some View {
+        modifier(TextStyle(size: 25, relativeTo: .headline))
+            .fontWeight(.light)
+            .foregroundColor(.text)
+    }
+    
+    func bodyStyle() -> some View {
+        modifier(TextStyle(size: 18, relativeTo: .body))
+            .fontWeight(.light)
+            .foregroundColor(.text)
+    }
+            
+    func footerStyle() -> some View {
+        modifier(TextStyle(size: 12, relativeTo: .footnote))
+            .fontWeight(.light)
+            .foregroundColor(.customSecondary)
+    }
+}
+
+struct TextStyles_Previews: PreviewProvider {
+    static var previews: some View {
+        TextStyles()
+    }
+}
