10d65260b3f8593313cd4eaea1c6bab84cf667b3
Tino Tusaumwe <tusatino@gmail.com>
2022-12-29 16:15:27 +1000

    Add EditProjectView

---
+++ IssueTracker/Views/EditProjectView.swift
@@ -0,0 +1,20 @@
+//
+//  EditProjectView.swift
+//  IssueTracker
+//
+//  Created by Tino on 29/12/2022.
+//
+
+import SwiftUI
+
+struct EditProjectView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct EditProjectView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProjectView()
+    }
+}
