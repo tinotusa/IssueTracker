ac8a2a6043e6a5e596c8481de22dc4648410dd7f
Tino Tusaumwe <tusatino@gmail.com>
2022-12-29 19:04:15 +1000

    Rename IssueTrackerTests to HomeViewTests

---
+++ IssueTrackerTests/HomeViewModelTests.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModelTests.swift
+//  HomeViewModelTests
+//
+//  Created by Tino on 26/12/2022.
+//
+
+import XCTest
+@testable import IssueTracker
+
+final class HomeViewModelTests: XCTestCase {
+}
