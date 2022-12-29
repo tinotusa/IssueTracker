107055cb2a0e3183b0f1ed7f4fc28b5bb8730e57
Tino Tusaumwe <tusatino@gmail.com>
2022-12-29 17:59:45 +1000

    Add EditProjectView

--- IssueTracker/Views/EditProjectView.swift
+++ IssueTracker/Views/EditProjectView.swift
@@ -6,15 +6,133 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct EditProjectView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.dismiss) private var dismiss
+
+    @FetchRequest(sortDescriptors: [.init(\.dateCreated, order: .reverse)])
+    var projects: FetchedResults<Project>
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationStack {
+            VStack {
+                header
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        Text("Select project")
+                            .titleStyle()
+                            .padding(.bottom)
+                        
+                        ForEach(projects) { project in
+                            NavigationLink(value: project) {
+                                ProjectRowView(project: project)
+                            }
+                        }
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                }
+            }
+            .padding()
+            .bodyStyle()
+            .background(Color.customBackground)
+            .navigationDestination(for: Project.self) { project in
+                EditView(project: project)
+                    .environment(\.managedObjectContext, viewContext)
+            }
+        }
     }
 }
 
+struct EditView: View {
+    @ObservedObject var project: Project
+    @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.dismiss) private var dismiss
+    @State private var name = ""
+    @State private var startDate = Date()
+    @State private var showingHasChangesConfirmationDialog = false
+    
+    init(project: Project) {
+        self.project = project
+        _name = State(wrappedValue: project.name ?? "Name not set")
+        _startDate = State(wrappedValue: project.startDate ?? Date())
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                PlainButton("Cancel", action: cancel)
+                Spacer()
+                PlainButton("Save", action: save)
+            }
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Text("Editing Project")
+                        .titleStyle()
+                    Text("Project name:")
+                    CustomTextField("Project name", text: $name)
+                    
+                    Text("Project start date:")
+                    DatePicker("Project start date", selection: $startDate, displayedComponents: [.date])
+                        .padding(.bottom)
+                    ProminentButton("Save changes", action: save)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                }
+            }
+        }
+        .padding()
+        .navigationBarBackButtonHidden(true)
+        .bodyStyle()
+        .confirmationDialog("Changes not saved.", isPresented: $showingHasChangesConfirmationDialog) {
+            Button("Dont save changes", role: .destructive) {
+                dismiss()
+            }
+        } message: {
+            Text("You have changes that haven't been saved.")
+        }
+    }
+    
+    func cancel() {
+        guard let projectName = project.name else {
+            return
+        }
+        guard let startDate = project.startDate else {
+            return
+        }
+        if self.name != projectName || self.startDate != startDate {
+            showingHasChangesConfirmationDialog = true
+        } else {
+            dismiss()
+        }
+    }
+    
+    func save() {
+        project.name = name
+        project.startDate = startDate
+        try? viewContext.save()
+        dismiss()
+    }
+}
+
+
+private extension EditProjectView {
+    var header: some View {
+        HStack {
+            Spacer()
+            PlainButton("Close") {
+                dismiss()
+            }
+        }
+    }
+}
 struct EditProjectView_Previews: PreviewProvider {
     static var previews: some View {
         EditProjectView()
+            .environment(
+                \.managedObjectContext,
+                 PersistenceController.projectsPreview.container.viewContext
+            )
+        
     }
 }
