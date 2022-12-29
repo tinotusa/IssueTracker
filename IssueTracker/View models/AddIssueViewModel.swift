aa228c6a549f5415a5cbdd03a5842971d18d61ac
Tino Tusaumwe <tusatino@gmail.com>
2022-12-29 22:24:01 +1000

    Add AddIssueView UI

--- IssueTracker/View models/AddIssueViewModel.swift
+++ IssueTracker/View models/AddIssueViewModel.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import os
+import CoreData
+
+final class AddIssueViewModel: ObservableObject {
+    @Published var name = ""
+    @Published var description = ""
+    @Published var priority: Issue.Priority = .low
+    @Published var tags: Set<Tag> = []
+    @Published var newTag = ""
+    private let log = Logger(subsystem: "com.tinotusa.IssueTracker", category: "AddIssueViewModel")
+    private let viewContext: NSManagedObjectContext
+    
+    init(viewContext: NSManagedObjectContext = PersistenceController.shared.container.viewContext) {
+        self.viewContext = viewContext
+    }
+}
+
+extension AddIssueViewModel {
+    var allFieldsFilled: Bool {
+        false
+    }
+    
+    func addIssue() {
+        guard allFieldsFilled else {
+            return
+        }
+    }
+    
+    /// Adds the given tag to the set of tags.
+    ///
+    /// If the tag already exists in the set it is removed.
+    ///
+    /// - Parameter tag: The tag to insert.
+    func addTag(_ tag: Tag) {
+        if tags.contains(tag) {
+            tags.remove(tag)
+        } else {
+            tags.insert(tag)
+        }
+    }
+}
--- IssueTracker/Views/AddIssueView.swift
+++ IssueTracker/Views/AddIssueView.swift
@@ -6,15 +6,91 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct AddIssueView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.dismiss) private var dismiss
+    @StateObject private var viewModel = AddIssueViewModel()
+    let project: Project
+    
+    @FetchRequest(sortDescriptors: [.init(\.dateCreated, order: .reverse)])
+    private var allTags: FetchedResults<Tag>
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading) {
+            header
+                .padding(.horizontal)
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Text("Add Issue")
+                        .titleStyle()
+                    LabeledInputField("Issue name:") {
+                        CustomTextField("Issue name", text: $viewModel.name)
+                    }
+                    LabeledInputField("Description:") {
+                        CustomTextField("Issue description", text: $viewModel.description)
+                    }
+                    LabeledInputField("Priority:") {
+                        Picker("Issue priority", selection: $viewModel.priority) {
+                            ForEach(Issue.Priority.allCases) { priority in
+                                Text(priority.title)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    LabeledInputField("Tags:") {
+                        HStack {
+                            CustomTextField("New tag", text: $viewModel.newTag)
+                            PlainButton("Add tag") {
+                                
+                            }
+                        }
+                        .padding(.bottom)
+                        LazyHGrid(rows: [.init(.adaptive(minimum: 100))]) {
+                            ForEach(allTags) { tag in
+                                TagButton(tag.name ?? "Not set") {
+                                    viewModel.addTag(tag)
+                                }
+                            }
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+            }
+            .safeAreaInset(edge: .bottom) {
+                ProminentButton("Add Issue") {
+                    
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                .disabled(!viewModel.allFieldsFilled)
+            }
+        }
+        .bodyStyle()
+        .background(Color.customBackground)
+    }
+}
+
+private extension AddIssueView {
+    var header: some View {
+        HStack {
+            PlainButton("Close") {
+                dismiss()
+            }
+            Spacer()
+            PlainButton("Add issue") {
+                
+            }
+            .disabled(!viewModel.allFieldsFilled)
+        }
     }
 }
 
 struct AddIssueView_Previews: PreviewProvider {
+    static var viewContext = PersistenceController.shared.container.viewContext
     static var previews: some View {
-        AddIssueView()
+        AddIssueView(project: .example(context: viewContext))
+            .environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
     }
 }
