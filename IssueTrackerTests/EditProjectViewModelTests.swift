012c032e6c44638bc900bff5460bf8cbe59cba11
Tino Tusaumwe <tusatino@gmail.com>
2022-12-29 20:22:26 +1000

    Add EditProjectViewModelTests

---
+++ IssueTrackerTests/EditProjectViewModelTests.swift
@@ -0,0 +1,21 @@
+//
+//  EditProjectViewModelTests.swift
+//  IssueTrackerTests
+//
+//  Created by Tino on 29/12/2022.
+//
+
+import XCTest
+@testable import IssueTracker
+
+final class EditProjectViewModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+}
