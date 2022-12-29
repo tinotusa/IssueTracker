e1a63826b1ad04b1a2dab9011d3a26cccc752262
Tino Tusaumwe <tusatino@gmail.com>
2022-12-29 19:42:21 +1000

    Add unit tests for AddProjectViewModel and HomeViewModel

--- IssueTrackerTests/HomeViewModelTests.swift
+++ IssueTrackerTests/HomeViewModelTests.swift
@@ -9,4 +9,20 @@ import XCTest
 @testable import IssueTracker
 
 final class HomeViewModelTests: XCTestCase {
+    var viewModel = HomeViewModel()
+    
+    override func setUpWithError() throws {
+        
+    }
+    
+    override func tearDownWithError() throws {
+        
+    }
+    
+    func testDefaultState() {
+        XCTAssertEqual(viewModel.title, "Projects", "Expected title to be Projects")
+        XCTAssertFalse(viewModel.showingAddProjectView, "Expected showingAddProjectView  to be false.")
+        XCTAssertFalse(viewModel.showingEditProjectView, "Expected showingEditProjectView to be false.")
+        XCTAssertFalse(viewModel.showingDeleteProjectView, "Expected showingDeleteProjectView to be false.")
+    }
 }
