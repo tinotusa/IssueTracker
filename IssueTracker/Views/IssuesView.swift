6de58c37ac937569000e5320ec17b42715b13983
Tino Tusaumwe <tusatino@gmail.com>
2022-12-29 21:38:38 +1000

    Add empty issues view UI

--- IssueTracker/Views/IssuesView.swift
+++ IssueTracker/Views/IssuesView.swift
@@ -6,15 +6,78 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct IssuesView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    private let project: Project
+    
+    @FetchRequest(sortDescriptors: [])
+    private var issues: FetchedResults<Issue>
+
+    init(project: Project) {
+        self.project = project
+        _issues = FetchRequest<Issue>(
+            sortDescriptors: [.init(\.dateCreated_, order: .reverse)],
+            predicate: NSPredicate(format: "%K == %@", "project", project)
+        )
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            header
+            
+            ScrollView {
+                VStack {
+                    if issues.isEmpty {
+                        Text("No issues to see.\nTap the Add issue button below to start.")
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.customSecondary)
+                    } else {
+                        ForEach(issues) { issue in
+                            Text(issue.name)
+                        }
+                    }
+                }
+            }
+            .safeAreaInset(edge: .bottom) {
+                ProminentButton("Add Issue") {
+                    
+                }
+            }
+        }
+        .padding()
+        .bodyStyle()
+        .background(Color.customBackground)
+    }
+}
+
+private extension IssuesView {
+    var header: some View {
+        HStack {
+            Text("Issues")
+                .titleStyle()
+            Spacer()
+            Menu {
+                Button("test") { }
+            } label: {
+                Text("Sort")
+            }
+            Menu {
+                Button("test") { }
+            } label: {
+                Text("Filter")
+            }
+        }
     }
 }
 
 struct IssuesView_Previews: PreviewProvider {
     static var previews: some View {
-        IssuesView()
+        IssuesView(project: .example(context: PersistenceController.shared.container.viewContext))
+            .environment(
+                \.managedObjectContext,
+                 PersistenceController.shared.container.viewContext
+            )
     }
 }
