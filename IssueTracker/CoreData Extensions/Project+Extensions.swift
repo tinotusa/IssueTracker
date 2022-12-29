60254e87a7470d06cc092cbbaeee4d7f28db8a18
Tino Tusaumwe <tusatino@gmail.com>
2022-12-29 16:10:11 +1000

    Add init and latestIssue to Project+Extensions
    Added a convenience init to make initializing Project objects easier.
    Re-added the latestIssue property since changing the code gen for
    Projects.

--- IssueTracker/CoreData Extensions/Project+Extensions.swift
+++ IssueTracker/CoreData Extensions/Project+Extensions.swift
@@ -5,4 +5,22 @@
 //  Created by Tino on 29/12/2022.
 //
 
-import Foundation
+import CoreData
+
+extension Project {
+    convenience init(name: String, startDate: Date, context: NSManagedObjectContext) {
+        self.init(context: context)
+        self.name = name
+        self.startDate = startDate
+    }
+    
+    public override func awakeFromInsert() {
+        self.id = UUID()
+        self.dateCreated = .now
+    }
+    
+    /// The latest issue added to the project.
+    var latestIssue: Issue? {
+        self.issues?.lastObject as? Issue
+    }
+}
