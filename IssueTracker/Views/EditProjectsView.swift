5c43f7d8b0348a301f800bde2e228d3f474d0c98
Tino Tusaumwe <tusatino@gmail.com>
2022-12-29 18:06:55 +1000

    Move EditProjectView to separate file

--- IssueTracker/Views/EditProjectsView.swift
+++ IssueTracker/Views/EditProjectsView.swift
@@ -38,83 +38,12 @@ struct EditProjectsView: View {
             .bodyStyle()
             .background(Color.customBackground)
             .navigationDestination(for: Project.self) { project in
-                EditView(project: project)
+                EditProjectView(project: project)
                     .environment(\.managedObjectContext, viewContext)
             }
         }
     }
 }
-
-struct EditView: View {
-    @ObservedObject var project: Project
-    @Environment(\.managedObjectContext) private var viewContext
-    @Environment(\.dismiss) private var dismiss
-    @State private var name = ""
-    @State private var startDate = Date()
-    @State private var showingHasChangesConfirmationDialog = false
-    
-    init(project: Project) {
-        self.project = project
-        _name = State(wrappedValue: project.name ?? "Name not set")
-        _startDate = State(wrappedValue: project.startDate ?? Date())
-    }
-    
-    var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                PlainButton("Cancel", action: cancel)
-                Spacer()
-                PlainButton("Save", action: save)
-            }
-            ScrollView {
-                VStack(alignment: .leading) {
-                    Text("Editing Project")
-                        .titleStyle()
-                    Text("Project name:")
-                    CustomTextField("Project name", text: $name)
-                    
-                    Text("Project start date:")
-                    DatePicker("Project start date", selection: $startDate, displayedComponents: [.date])
-                        .padding(.bottom)
-                    ProminentButton("Save changes", action: save)
-                    .frame(maxWidth: .infinity, alignment: .center)
-                }
-            }
-        }
-        .padding()
-        .navigationBarBackButtonHidden(true)
-        .bodyStyle()
-        .confirmationDialog("Changes not saved.", isPresented: $showingHasChangesConfirmationDialog) {
-            Button("Dont save changes", role: .destructive) {
-                dismiss()
-            }
-        } message: {
-            Text("You have changes that haven't been saved.")
-        }
-    }
-    
-    func cancel() {
-        guard let projectName = project.name else {
-            return
-        }
-        guard let startDate = project.startDate else {
-            return
-        }
-        if self.name != projectName || self.startDate != startDate {
-            showingHasChangesConfirmationDialog = true
-        } else {
-            dismiss()
-        }
-    }
-    
-    func save() {
-        project.name = name
-        project.startDate = startDate
-        try? viewContext.save()
-        dismiss()
-    }
-}
-
 
 private extension EditProjectsView {
     var header: some View {
@@ -126,7 +55,8 @@ private extension EditProjectsView {
         }
     }
 }
-struct EditProjectView_Previews: PreviewProvider {
+
+struct EditProjectsView_Previews: PreviewProvider {
     static var previews: some View {
         EditProjectsView()
             .environment(
