b14afe23f92ebbbec2b227b75ada6f6ec44b4585
Tino Tusaumwe <tusatino@gmail.com>
2022-12-30 17:23:27 +1000

    Add IssueDetail

---
+++ IssueTracker/Views/IssueDetail.swift
@@ -0,0 +1,20 @@
+//
+//  IssueDetail.swift
+//  IssueTracker
+//
+//  Created by Tino on 30/12/2022.
+//
+
+import SwiftUI
+
+struct IssueDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IssueDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        IssueDetail()
+    }
+}
