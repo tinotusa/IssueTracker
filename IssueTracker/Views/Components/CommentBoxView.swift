2f91a7f0c9ff547f30eed4ffe839525f831039e0
Tino Tusaumwe <tusatino@gmail.com>
2022-12-30 19:04:01 +1000

    Add CommentBoxView UI

--- IssueTracker/Views/Components/CommentBoxView.swift
+++ IssueTracker/Views/Components/CommentBoxView.swift
@@ -8,13 +8,82 @@
 import SwiftUI
 
 struct CommentBoxView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @ObservedObject var comment: Comment
+    let issue: Issue
+    @State private var isEditing = false
+    @State private var showingDeleteConfirmation = false
+    @State private var originalComment = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            if isEditing {
+                TextEditor(text: $comment.comment)
+                    .scrollContentBackground(.hidden)
+                    .frame(minHeight: 30)
+            } else {
+                Text(comment.comment)
+            }
+            HStack {
+                Text(comment.dateCreated.formatted(date: .abbreviated, time: .omitted))
+                    .footerStyle()
+                Spacer()
+                if isEditing {
+                    Button("Save") {
+                        issue.addToComments(comment)
+                        withAnimation {
+                            isEditing = false
+                        }
+                        try? viewContext.save()
+                    }
+                    Button("Cancel") {
+                        comment.comment = originalComment
+                        withAnimation {
+                            isEditing = false
+                        }
+                    }
+                } else {
+                    Button {
+                        originalComment = comment.comment
+                        withAnimation {
+                            isEditing = true
+                        }
+                    } label: {
+                        Image(systemName: "rectangle.and.pencil.and.ellipsis")
+                    }
+                }
+                if !isEditing {
+                    Button {
+                        showingDeleteConfirmation = true
+                    } label: {
+                        Image(systemName: "trash")
+                    }
+                }
+            }
+        }
+        .padding()
+        .background(Color.popup)
+        .cornerRadius(10)
+        .confirmationDialog("Delete comment", isPresented: $showingDeleteConfirmation) {
+            Button("Delete", role: .destructive) {   
+                issue.removeFromComments(comment)
+                withAnimation {
+                    try? viewContext.save()
+                }
+            }
+        } message: {
+            Text("Delete this comment.")
+        }
     }
 }
 
 struct CommentBoxView_Previews: PreviewProvider {
+    static let viewContext = PersistenceController.issuesPreview.container.viewContext
+    
     static var previews: some View {
-        CommentBoxView()
+        CommentBoxView(
+            comment: .init(comment: "testing", context: viewContext),
+            issue: .init(name: "test", issueDescription: "", priority: .low, tags: [], context: viewContext)
+        )
     }
 }
