d19f9043b36a37d6b6458dd2d9ccd080a8b6be21
Tino Tusaumwe <tusatino@gmail.com>
2022-12-30 19:03:41 +1000

    Add CommentBoxView

---
+++ IssueTracker/Views/Components/CommentBoxView.swift
@@ -0,0 +1,20 @@
+//
+//  CommentBoxView.swift
+//  IssueTracker
+//
+//  Created by Tino on 30/12/2022.
+//
+
+import SwiftUI
+
+struct CommentBoxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CommentBoxView_Previews: PreviewProvider {
+    static var previews: some View {
+        CommentBoxView()
+    }
+}
