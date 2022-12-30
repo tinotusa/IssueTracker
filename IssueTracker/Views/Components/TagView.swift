b43f6c1f29b82496d4ae56fd8d8254cf5aae1a27
Tino Tusaumwe <tusatino@gmail.com>
2022-12-30 19:08:07 +1000

    Add TagView

---
+++ IssueTracker/Views/Components/TagView.swift
@@ -0,0 +1,20 @@
+//
+//  TagView.swift
+//  IssueTracker
+//
+//  Created by Tino on 30/12/2022.
+//
+
+import SwiftUI
+
+struct TagView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct TagView_Previews: PreviewProvider {
+    static var previews: some View {
+        TagView()
+    }
+}
