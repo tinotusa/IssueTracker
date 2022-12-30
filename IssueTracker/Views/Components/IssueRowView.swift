00f2e5742fbb6b95f2edbd6e15d3ebb0c77c5dd9
Tino Tusaumwe <tusatino@gmail.com>
2022-12-30 14:02:47 +1000

    Add IssueRowView

---
+++ IssueTracker/Views/Components/IssueRowView.swift
@@ -0,0 +1,20 @@
+//
+//  IssueRowView.swift
+//  IssueTracker
+//
+//  Created by Tino on 30/12/2022.
+//
+
+import SwiftUI
+
+struct IssueRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IssueRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        IssueRowView()
+    }
+}
