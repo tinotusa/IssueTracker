ab26cfbcdf6bf450ec30d0d5694b94ee52e369a9
Tino Tusaumwe <tusatino@gmail.com>
2022-12-31 21:11:02 +1000

    Add FilteredIssuesListView

---
+++ IssueTracker/Views/Components/FilteredIssuesListView.swift
@@ -0,0 +1,20 @@
+//
+//  FilteredIssuesListView.swift
+//  IssueTracker
+//
+//  Created by Tino on 31/12/2022.
+//
+
+import SwiftUI
+
+struct FilteredIssuesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilteredIssuesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilteredIssuesListView()
+    }
+}
