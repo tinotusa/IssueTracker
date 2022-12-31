be6192a29b50d56366d5d92994f1de0cc3bad814
Tino Tusaumwe <tusatino@gmail.com>
2022-12-31 10:48:17 +1000

    Add forms to edit Issue properties

--- IssueTracker/View models/IssueEditViewModel.swift
+++ IssueTracker/View models/IssueEditViewModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import CoreData
+
+final class IssueEditViewModel: ObservableObject {
+    @Published var issueCopy: Issue
+    
+    init(issue: Issue, viewContext: NSManagedObjectContext = PersistenceController.shared.container.viewContext) {
+        issueCopy = Issue(
+            name: issue.name,
+            issueDescription: issue.description,
+            priority: issue.priority,
+            tags: issue.tags?.set as! Set<Tag>,
+            context: issue.managedObjectContext!
+        )
+    }
+}
+
+extension IssueEditViewModel {
+    func saveChanges() {
+        
+    }
+}
--- IssueTracker/Views/IssueEditView.swift
+++ IssueTracker/Views/IssueEditView.swift
@@ -8,13 +8,63 @@
 import SwiftUI
 
 struct IssueEditView: View {
+    @ObservedObject var issue: Issue
+    
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.managedObjectContext) private var viewContext
+    @StateObject private var viewModel: IssueEditViewModel
+    
+    init(issue: Issue) {
+        self.issue = issue
+        _viewModel = StateObject(wrappedValue: IssueEditViewModel(issue: issue))
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            header
+                .padding([.horizontal, .top])
+            
+            ScrollView {
+                VStack(alignment: .leading) {
+                    LabeledInputField("Issue name:") {
+                        CustomTextField("Issue name", text: $viewModel.issueCopy.name)
+                    }
+                    LabeledInputField("Description:") {
+                        CustomTextField("Issue Description", text: $viewModel.issueCopy.issueDescription)
+                    }
+                    LabeledInputField("Priority:") {
+                        Picker("Issue priority", selection: $viewModel.issueCopy.priority) {
+                            ForEach(Issue.Priority.allCases) { priority in
+                                Text(priority.title)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    LabeledInputField("Tags:") {
+                        TagFilterView(selectedTags: viewModel.issueCopy.tags?.set as? Set<Tag> ?? [])
+                    }
+                }
+                .padding(.horizontal)
+            }
+        }
     }
 }
-
+private extension IssueEditView {
+    var header: some View {
+        HStack {
+            Button("Close") {
+                dismiss()
+            }
+            Spacer()
+            Button("Save") {
+                
+            }
+        }
+    }
+}
 struct IssueEditView_Previews: PreviewProvider {
+    static let viewContext = PersistenceController.issuesPreview.container.viewContext
     static var previews: some View {
-        IssueEditView()
+        IssueEditView(issue: Issue(name: "Test issue", issueDescription: "", priority: .low, tags: [], context: viewContext))
     }
 }
