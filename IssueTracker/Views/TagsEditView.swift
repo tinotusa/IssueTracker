6d73f48e3e99a9dc646dc181277905d4164ef657
Tino Tusaumwe <tusatino@gmail.com>
2023-01-02 18:37:19 +1000

    Add TagsEditView

---
+++ IssueTracker/Views/TagsEditView.swift
@@ -0,0 +1,20 @@
+//
+//  TagsEditView.swift
+//  IssueTracker
+//
+//  Created by Tino on 2/1/2023.
+//
+
+import SwiftUI
+
+struct TagsEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TagsEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        TagsEditView()
+    }
+}
