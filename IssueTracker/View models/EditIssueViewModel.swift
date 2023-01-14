37cc4c41735e7b55f5fdf10fb8986c366c4319da
Tino Tusaumwe <tusatino@gmail.com>
2023-01-14 16:24:10 +1000

    Fix hasChanges logic for EditIssueViewmodel

--- IssueTracker/View models/EditIssueViewModel.swift
+++ IssueTracker/View models/EditIssueViewModel.swift
@@ -10,12 +10,19 @@ import CoreData
 import os
 
 final class EditIssueViewModel: ObservableObject {
+    /// A copy of the issue being edited.
     @Published var issueCopy: Issue
+    /// A set of the tags the user has selected when editing.
     @Published var selectedTags: Set<Tag> = []
+    /// The issue being edited.
     private var issue: Issue
     private let log = Logger(subsystem: "com.tinotusa.IssueTracker", category: "IssueEditViewModel")
     private let viewContext: NSManagedObjectContext
     
+    /// Creates a new EditIssueViewModel
+    /// - Parameters:
+    ///   - issue: The issue being edited.
+    ///   - viewContext: The managed object context for the issue.
     init(issue: Issue, viewContext: NSManagedObjectContext = PersistenceController.shared.container.viewContext) {
         self.issue = issue
         self.viewContext = viewContext
@@ -31,15 +38,24 @@ final class EditIssueViewModel: ObservableObject {
 }
 
 extension EditIssueViewModel {
+    /// A boolean value indicating that changes have been made.
     var hasChanges: Bool {
+        guard let issueTags = issue.tags else {
+            return false
+        }
+        // check that all the tags selected are equal to the original tags.
+        // if they are not the same a change has been made.
+        let allTagsAreEqual = selectedTags.allSatisfy { issueTags.contains($0) }
         return (
             issue.name != issueCopy.name ||
             issue.issueDescription != issueCopy.issueDescription ||
-            issue.tags != issueCopy.tags ||
-            issue.priority != issueCopy.priority
+            issue.priority != issueCopy.priority ||
+            issueTags.count != selectedTags.count ||
+            !allTagsAreEqual
         )
     }
     
+    /// Saves the changes made to core data.
     func saveChanges() {
         log.debug("Saving changes ...")
         if !hasChanges {
