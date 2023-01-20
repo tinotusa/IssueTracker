0d0afa93c6cc119c2557467820cfaaae0bbaf4eb
Tino Tusaumwe <tusatino@gmail.com>
2023-01-20 21:08:44 +1000

    Add SFSymbol to hold apps sf symbols

---
+++ MacIssueTracker/Models/SFSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  SFSymbol.swift
+//  MacIssueTracker
+//
+//  Created by Tino on 18/1/2023.
+//
+
+import SwiftUI
+
+/// Sf symbols used in the app.
+enum SFSymbol: String, CaseIterable, Identifiable {
+    case trash = "trash"
+    case plus = "plus"
+    case sidebarLeft = "sidebar.left"
+    case squareAndPencil = "square.and.pencil"
+    case rectangleAndPencilAndEllipsis = "rectangle.and.pencil.and.ellipsis"
+    
+    /// A unique id of a case.
+    var id: Self { self }
+    
+    /// The image of the sf symbol.
+    var image: Image { Image(systemName: self.rawValue) }
+}
+    
