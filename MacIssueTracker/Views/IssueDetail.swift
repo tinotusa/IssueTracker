dae761f728687cb13103562a8c4e8f174a5e3d82
Tino Tusaumwe <tusatino@gmail.com>
2023-01-21 18:15:43 +1000

    Refactor and clean IssueDetail code

--- MacIssueTracker/Views/IssueDetail.swift
+++ MacIssueTracker/Views/IssueDetail.swift
@@ -17,128 +17,136 @@ struct IssueDetail: View {
     }
     
     var body: some View {
-            Form {
-                Text(issue.name)
-                    .font(.system(size: 30))
-                Text("Created: \(issue.dateCreated.formatted(date: .abbreviated, time: .shortened))")
+        Form {
+            Text(issue.name)
+                .titleStyle()
+            Text("Created: \(issue.dateCreated.formatted(date: .abbreviated, time: .shortened))")
+                .foregroundColor(.secondary)
+            descriptionSection
+            prioritySection
+            tagsSection
+            commentsSection
+        }
+        .bodyStyle()
+        .safeAreaInset(edge: .bottom) {
+            HStack {
+                TextEditor(text: $viewModel.comment)
+                    .frame(maxHeight: 100)
+                Button("Add comment") {
+                    viewModel.addComment()
+                }
+                .disabled(viewModel.comment.isEmpty)
+            }
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Button("Edit") {
+                    // TODO: implement
+                }
+            }
+        }
+    }
+}
+
+// MARK: - Subviews
+private extension IssueDetail {
+    var descriptionSection: some View {
+        Section {
+            Group {
+                if issue.issueDescription.isEmpty {
+                    Text("No Description")
+                } else {
+                    Text(issue.issueDescription)
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(.gray)
+            }
+        } header: {
+            Text("Description")
+                .titleStyle()
+        }
+    }
+    
+    var prioritySection: some View {
+        Section {
+            Text(issue.priority.title)
+        } header: {
+            Text("Priority")
+                .titleStyle()
+        }
+    }
+    
+    var tagsSection: some View {
+        Section {
+            #warning("Add tags as set property")
+            if let tags = issue.tags?.set as? Set<Tag>, tags.isEmpty {
+                Text("No tags")
                     .foregroundColor(.secondary)
-                Section {
-                    if issue.issueDescription.isEmpty {
-                        Text("No Description")
-                            .font(.system(size: 20))
-                            .padding()
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .background {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .foregroundColor(.gray)
-                                    
-                            }
-                    } else {
-                        Text(issue.issueDescription)
-                            .font(.system(size: 20))
-                            .padding()
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .background {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .foregroundColor(.gray)
-                                    
-                            }
+            } else {
+                if let tags = issue.tags?.set as? Set<Tag> {
+                    ForEach(Array(tags)) { tag in
+                        Text(tag.name)
                     }
-                } header: {
-                    Text("Description")
-                        .font(.system(size: 30))
                 }
-                
-                Section {
-                    Text(issue.priority.title)
-                        .font(.system(size: 20))
-                } header: {
-                    Text("Priority")
-                        .font(.system(size: 30))
-                }
-                
-                Section {
-                    if let tags = issue.tags?.set as? Set<Tag>, tags.isEmpty {
-                        Text("No tags")
-                            .foregroundColor(.secondary)
-                            .font(.system(size: 20))
-                    } else {
-                        if let tags = issue.tags?.set as? Set<Tag> {
-                            ForEach(Array(tags)) { tag in
-                                Text(tag.name)
-                                    .font(.system(size: 20))
-                            }
-                        }
-                    }
-                } header: {
-                    Text("Tags")
-                        .font(.system(size: 30))
-                }
-                
-                Section {
-                    if let comments = issue.comments?.set as? Set<Comment>, comments.isEmpty {
-                        Text("No comments")
-                            .foregroundColor(.secondary)
-                    } else {
-                        ScrollView {
-                            VStack(alignment: .leading) {
-                                if let comments = issue.comments?.set as? Set<Comment> {
-                                    ForEach(Array(comments)) { comment in
-                                        VStack {
-                                            Text(comment.comment)
-                                                .font(.system(size: 20))
-                                            HStack {
-                                                Spacer()
-                                                Button {
-                                                    // edit button
-                                                } label: {
-                                                    Label("Edit", systemImage: "rectangle.and.pencil.and.ellipsis")
-                                                }
-                                                Button(role: .destructive) {
-                                                    // delete code
-                                                } label: {
-                                                    Label("Delete", systemImage: "trash")
-                                                }
-                                            }
+            }
+        } header: {
+            Text("Tags")
+                .titleStyle()
+        }
+    }
+    
+    var commentsSection: some View {
+        Section {
+            if let comments = issue.comments?.set as? Set<Comment>, comments.isEmpty {
+                Text("No comments")
+                    .foregroundColor(.secondary)
+            } else {
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        if let comments = issue.comments?.set as? Set<Comment> {
+                            #warning("Add comments view")
+                            ForEach(Array(comments)) { comment in
+                                VStack {
+                                    Text(comment.comment)
+                                    HStack {
+                                        Spacer()
+                                        Button {
+                                            // edit button
+                                        } label: {
+                                            Label("Edit", systemImage: "rectangle.and.pencil.and.ellipsis")
                                         }
-                                        .padding()
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                        .background {
-                                            RoundedRectangle(cornerRadius: 10)
-                                                .foregroundColor(.gray)
+                                        Button(role: .destructive) {
+                                            // delete code
+                                        } label: {
+                                            Label("Delete", systemImage: "trash")
                                         }
                                     }
+                                }
+                                .padding()
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .background {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .foregroundColor(.gray)
                                 }
                             }
                         }
                     }
-                } header: {
-                    Text("Comments")
-                        .font(.system(size: 30))
                 }
             }
-            .safeAreaInset(edge: .bottom) {
-                HStack {
-                    TextEditor(text: $viewModel.comment)
-                        .frame(maxHeight: 100)
-                    Button("Add comment") {
-                        viewModel.addComment()
-                    }
-                    .disabled(viewModel.comment.isEmpty)
-                }
-            }
-            .padding()
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-            .toolbar {
-                ToolbarItem(placement: .primaryAction) {
-                    Button("Edit") {
-                        
-                    }
-                }
-            }
+        } header: {
+            Text("Comments")
+                .titleStyle()
+        }
     }
 }
 
+// MARK: - Previews
 struct IssueDetail_Previews: PreviewProvider {
     static var viewContext = PersistenceController.projectsPreview.container.viewContext
     static var issue = Issue(name: "some issue", issueDescription: "", priority: .low, tags: [], context: viewContext)
