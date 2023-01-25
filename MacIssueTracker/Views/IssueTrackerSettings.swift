4e9eb9648b41fdcb3c5ee9fddcffe3cc9ead91e5
Tino Tusaumwe <tusatino@gmail.com>
2023-01-25 13:33:28 +1000

    Add IssueTracker settings

---
+++ MacIssueTracker/Views/IssueTrackerSettings.swift
@@ -0,0 +1,20 @@
+//
+//  IssueTrackerSettings.swift
+//  MacIssueTracker
+//
+//  Created by Tino on 25/1/2023.
+//
+
+import SwiftUI
+
+struct IssueTrackerSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IssueTrackerSettings_Previews: PreviewProvider {
+    static var previews: some View {
+        IssueTrackerSettings()
+    }
+}
