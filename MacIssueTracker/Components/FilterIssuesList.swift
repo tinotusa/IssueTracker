d8fd0ccfc3d5444852165ae9479b3c91dee64329
Tino Tusaumwe <tusatino@gmail.com>
2023-01-26 16:47:49 +1000

    Add FilterIssuesList

---
+++ MacIssueTracker/Components/FilterIssuesList.swift
@@ -0,0 +1,20 @@
+//
+//  FilterIssuesList.swift
+//  MacIssueTracker
+//
+//  Created by Tino on 26/1/2023.
+//
+
+import SwiftUI
+
+struct FilterIssuesList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilterIssuesList_Previews: PreviewProvider {
+    static var previews: some View {
+        FilterIssuesList()
+    }
+}
