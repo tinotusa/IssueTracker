6705084d2e4819811e4f040bf387e3da9497a370
Tino Tusaumwe <tusatino@gmail.com>
2023-01-26 16:49:33 +1000

    Add FilterIssuesList UI

--- MacIssueTracker/Components/FilterIssuesList.swift
+++ MacIssueTracker/Components/FilterIssuesList.swift
@@ -7,14 +7,50 @@
 
 import SwiftUI
 
-struct FilterIssuesList: View {
+struct FilterIssuesList<Content: View>: View {
+    let content: (Issue) -> Content
+    @Binding var selectedIssue: Issue?
+    
+    @FetchRequest(sortDescriptors: [])
+    private var issues: FetchedResults<Issue>
+    
+    init(
+        selection selectedIssue: Binding<Issue?>,
+        predicate: NSPredicate,
+        @ViewBuilder content: @escaping (Issue) -> Content
+    ) {
+        _selectedIssue = selectedIssue
+        self.content = content
+        _issues = FetchRequest(
+            sortDescriptors: [.init(\.dateCreated_, order: .forward)],
+            predicate: predicate
+        )
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(selection: $selectedIssue) {
+            Text("Issues")
+                .foregroundColor(.secondary)
+            if issues.isEmpty {
+                Text("No issues found.")
+            } else {
+                ForEach(issues) { issue in
+                    content(issue)
+                }
+            }
+        }
     }
 }
 
 struct FilterIssuesList_Previews: PreviewProvider {
+    static var viewContext = PersistenceController.projectsPreview.container.viewContext
+    static var project = Project.example(context: viewContext)
+    static var predicate = NSPredicate(format: "project == %@ && status_ == %@", project, "open")
+    
     static var previews: some View {
-        FilterIssuesList()
+        FilterIssuesList(selection: .constant(nil), predicate: predicate) { issue in
+            IssueRow(issue: issue)
+        }
+        .environment(\.managedObjectContext, viewContext)
     }
 }
