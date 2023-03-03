eee624969add1746b88dee233c297f715ee96eef
Tino Tusaumwe <tusatino@gmail.com>
2023-03-03 13:36:32 +1000

    Add AddCommentView

---
+++ IssueTracker/Views/AddCommentView.swift
@@ -0,0 +1,20 @@
+//
+//  AddCommentView.swift
+//  IssueTracker
+//
+//  Created by Tino on 1/3/2023.
+//
+
+import SwiftUI
+
+struct AddCommentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddCommentView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddCommentView()
+    }
+}
