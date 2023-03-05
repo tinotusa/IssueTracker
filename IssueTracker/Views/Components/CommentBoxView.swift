074f9326d929d7cb9c7d42a948815e30191d9367
Tino Tusaumwe <tusatino@gmail.com>
2023-03-05 23:29:00 +1000

    Add logic to load attachment data from iCloud

--- IssueTracker/Views/Components/CommentBoxView.swift
+++ IssueTracker/Views/Components/CommentBoxView.swift
@@ -6,6 +6,124 @@
 //
 
 import SwiftUI
+import CloudKit
+
+struct ImageAttachmentIcon: View {
+    let url: URL
+    @State private var imageURL: URL?
+    
+    var body: some View {
+        Group {
+            if let imageURL {
+                AsyncImage(url: imageURL)
+            } else {
+                ProgressView()
+                    .progressViewStyle(.circular)
+            }
+        }
+        .frame(width: 50, height: 50)
+        .cornerRadius(10)
+        .task {
+            let query = CKQuery(
+                recordType: "Attachment",
+                predicate: .init(format: "attachmentURL == %@", url.absoluteString)
+            )
+            
+            let operation = CKQueryOperation(query: query)
+            operation.resultsLimit = 1
+            operation.recordMatchedBlock = { id, result in
+                switch result {
+                case .failure(let error):
+                    print("Error failed to get asset: \(error)")
+                case .success(let record):
+                    guard let asset = record["attachment"] as? CKAsset,
+                          let url = asset.fileURL
+                    else {
+                        print("failed to get asset or url")
+                        return
+                    }
+                    self.imageURL = url
+                }
+            }
+            // TODO: change to use results block
+            operation.start()
+        }
+    }
+}
+
+struct AudioAttachmentIcon: View {
+    let url: URL
+    @State private var audioURL: URL?
+    @StateObject private var audioPlayer = AudioPlayer()
+    
+    var body: some View {
+        Group {
+            if audioURL != nil {
+                VStack {
+                    Button {
+                        if audioPlayer.isPlaying {
+                            audioPlayer.stop()
+                        } else {
+                            audioPlayer.play()
+                        }
+                    } label: {
+                        Image(systemName: audioPlayer.isPlaying ? "stop.circle.fill" : "play.circle.fill")
+                            .font(.title)
+                    }
+                    Image(systemName: "waveform")
+                        .foregroundColor(audioPlayer.isPlaying ? .blue : .secondary)
+                }
+                .onAppear {
+                    audioPlayer.setUpPlayer(url: URL(filePath: audioURL!.path()))
+                }
+                .padding(.horizontal)
+                .padding(.vertical, 10)
+                .background(.white)
+                .cornerRadius(10)
+            } else {
+                ProgressView()
+                    .progressViewStyle(.circular)
+                    .frame(width: 50, height: 50)
+            }
+        }
+        .onAppear {
+            print("audio attachment appeared")
+            let query = CKQuery(
+                recordType: "Attachment",
+                predicate: .init(format: "attachmentURL == %@", url.absoluteString)
+            )
+            var url: URL? = nil
+            let operation = CKQueryOperation(query: query)
+            operation.resultsLimit = 1
+            operation.recordMatchedBlock = { id, result in
+                print("in match block")
+                switch result {
+                case .failure(let error):
+                    print("Failed to get audio record. \(error)")
+                case .success(let record):
+                    guard let asset = record["attachment"] as? CKAsset else {
+                        print("failed to get audio asset")
+                        return
+                    }
+                    url = asset.fileURL
+                    print("Successfully got the audio url: \(url)")
+                }
+            }
+            operation.queryResultBlock = { result in
+                switch result {
+                case.success(let cursor):
+                    print("successfully got the data \(cursor)")
+                    self.audioURL = url
+                case .failure(let error):
+                    print("failed to get audio from cloudkit. \(error)")
+                }
+            }
+            print("set icloud record block")
+            operation.start()
+            print("after opertation start")
+        }
+    }
+}
 
 struct CommentBoxView: View {
     @Environment(\.managedObjectContext) private var viewContext
@@ -23,6 +141,22 @@ struct CommentBoxView: View {
                     .frame(minHeight: 30)
             } else {
                 Text(comment.comment)
+                
+                ScrollView(.horizontal) {
+                    HStack {
+                        ForEach(comment.wrappedAttachments) { attachment in
+                            let attachmentType = AttachmentType(rawValue: attachment.type_)!
+                            switch attachmentType {
+                            case .audio:
+                                AudioAttachmentIcon(url: attachment.assetURL_!)
+                            case .image:
+                                ImageAttachmentIcon(url: attachment.assetURL_!)
+                            case .video:
+                                Text("TODO")
+                            }
+                        }
+                    }
+                }
             }
             HStack {
                 Text(comment.dateCreated.formatted(date: .abbreviated, time: .omitted))
@@ -67,6 +201,7 @@ struct CommentBoxView: View {
         .confirmationDialog("Delete comment", isPresented: $showingDeleteConfirmation) {
             Button("Delete", role: .destructive) {   
                 issue.removeFromComments(comment)
+                // TODO: Delete the attachment files as well.
                 withAnimation {
                     try? viewContext.save()
                 }
