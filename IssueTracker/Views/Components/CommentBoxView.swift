864f8276983306f6a9f12cb878e3297877f30645
Tino Tusaumwe <tusatino@gmail.com>
2023-03-20 17:55:10 +1000

    Remove add and edit buttons on CommentBoxView

--- IssueTracker/Views/Components/CommentBoxView.swift
+++ IssueTracker/Views/Components/CommentBoxView.swift
@@ -8,9 +8,6 @@
 import SwiftUI
 
 struct CommentBoxView: View {
-    @State private var isEditing = false
-    @State private var showingDeleteConfirmation = false
-    @State private var originalComment = ""
     @State private var showingAttachments = false
     
     @ObservedObject private(set) var comment: Comment
@@ -20,95 +17,45 @@ struct CommentBoxView: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            if isEditing {
-                TextEditor(text: $comment.wrappedComment)
-                    .scrollContentBackground(.hidden)
-                    .frame(minHeight: 30)
-            } else {
-                Text(comment.wrappedComment)
-                if !comment.wrappedAttachments.isEmpty {
+            Text(comment.wrappedComment)
+            
+            if !comment.hasAttachments {
+                HStack {
+                    Text("^[\(comment.wrappedAttachments.count) Attachment](inflect: true)")
+                        .foregroundColor(.secondary)
+                    Button(showingAttachments ? "Hide" : "Show") {
+                        withAnimation {
+                            showingAttachments.toggle()
+                        }
+                    }
+                    .foregroundColor(.blue)
+                }
+                .font(.footnote)
+            }
+            if showingAttachments {
+                ScrollView(.horizontal) {
                     HStack {
-                        Text("^[\(comment.wrappedAttachments.count) Attachment](inflect: true)")
-                            .foregroundColor(.secondary)
-                        Button(showingAttachments ? "Hide" : "Show") {
-                            withAnimation {
-                                showingAttachments.toggle()
-                            }
-                        }
-                        .foregroundColor(.blue)
-                    }
-                    .font(.footnote)
-                }
-                if showingAttachments {
-                    ScrollView(.horizontal) {
-                        HStack {
-                            ForEach(comment.sortedAttachments) { attachment in
-                                let attachmentType = AttachmentType(rawValue: attachment.type)!
-                                let url = attachment.assetURL!
-                                switch attachmentType {
-                                case .audio:
-                                    AudioAttachmentView(url: url)
-                                case .image:
-                                    ImageAttachmentView(url: url)
-                                case .video:
-                                    Text("TODO")
-                                }
+                        ForEach(comment.sortedAttachments) { attachment in
+                            let attachmentType = AttachmentType(rawValue: attachment.type)!
+                            let url = attachment.assetURL!
+                            switch attachmentType {
+                            case .audio:
+                                AudioAttachmentView(url: url)
+                            case .image:
+                                ImageAttachmentView(url: url)
+                            case .video:
+                                Text("TODO")
                             }
                         }
                     }
                 }
             }
-            HStack {
-                Text(comment.wrappedDateCreated.formatted(date: .abbreviated, time: .omitted))
-                    .footerStyle()
-                Spacer()
-                if isEditing {
-                    Button("Save") {
-                        issue.addToComments(comment)
-                        withAnimation {
-                            isEditing = false
-                        }
-                        try? viewContext.save()
-                    }
-                    Button("Cancel") {
-                        comment.comment = originalComment
-                        withAnimation {
-                            isEditing = false
-                        }
-                    }
-                } else {
-                    Button {
-                        originalComment = comment.wrappedComment
-                        withAnimation {
-                            isEditing = true
-                        }
-                    } label: {
-                        Image(systemName: "rectangle.and.pencil.and.ellipsis")
-                    }
-                }
-                if !isEditing {
-                    Button {
-                        showingDeleteConfirmation = true
-                    } label: {
-                        Image(systemName: "trash")
-                    }
-                }
-            }
+            Text(comment.wrappedDateCreated.formatted(date: .abbreviated, time: .omitted))
+                .footerStyle()
         }
         .padding()
         .background(Color.popup)
         .cornerRadius(10)
-        .confirmationDialog("Delete comment", isPresented: $showingDeleteConfirmation) {
-            Button("Delete", role: .destructive) {   
-                issue.removeFromComments(comment)
-                // TODO: Delete the attachment files as well.
-                withAnimation {
-                    try? viewContext.save()
-                }
-            }
-        } message: {
-            Text("Delete this comment.")
-        }
     }
 }
 
