9d32489225278a4bc3ecefc4361a50a9876858d1
Tino Tusaumwe <tusatino@gmail.com>
2023-04-07 13:41:56 +1000

    Add documentation comments

--- IssueTracker/Persistence.swift
+++ IssueTracker/Persistence.swift
@@ -45,6 +45,13 @@ class PersistenceController {
 
 // MARK: - Issue functions
 extension PersistenceController {
+    /// Adds a new issue to core data.
+    /// - Parameters:
+    ///   - name: The name of the issue.
+    ///   - issueDescription: The description of the issue.
+    ///   - priority: The priority of the issue.
+    ///   - tags: The tags associated with the issue.
+    ///   - project: The project the issue is a part of.
     func addIssue(
         name: String,
         issueDescription: String,
@@ -53,10 +60,13 @@ extension PersistenceController {
         project: Project
     ) throws {
         let issue = Issue(name: name, issueDescription: issueDescription, priority: priority, tags: tags, context: viewContext)
+        logger.debug("Adding new issue with id: \(issue.wrappedId)")
         project.addToIssues(issue)
         try save()
     }
     
+    /// Toggles the issues status.
+    /// - Parameter issue: The issue to toggle.
     func toggleIssueStatus(for issue: Issue) throws {
         switch issue.wrappedStatus {
         case .open: issue.wrappedStatus = .closed
@@ -65,6 +75,10 @@ extension PersistenceController {
         try save()
     }
     
+    /// Changes the issues status to the given status.
+    /// - Parameters:
+    ///   - issue: The issue to change.
+    ///   - status: The status to set the issue to.
     func setIssueStatus(for issue: Issue, to status: Issue.Status) throws {
         if issue.wrappedStatus == status {
             return
@@ -73,7 +87,13 @@ extension PersistenceController {
         try save()
     }
     
+    /// Copies values from the source issue to the destination issue.
+    /// - Parameters:
+    ///   - source: The issue to copy from.
+    ///   - destination: The issue to copy to.
+    ///   - tags: The tags from the source issue to copy to the destination.
     func copyIssue(from source: Issue, to destination: Issue, withTags tags: Set<Tag>) throws {
+        logger.debug("Copying from issue: \(source.wrappedId) to issue: \(destination.wrappedId)")
         destination.copyProperties(from: source)
         destination.tags = .init(set: tags)
         
@@ -81,15 +101,26 @@ extension PersistenceController {
     }
 }
 
-#warning("add logging to these functions")
 extension PersistenceController {
+    /// The managed object context for the container.
     var viewContext: NSManagedObjectContext {
         Self.shared.container.viewContext
     }
     
-    func addComment(comment: String, to issue: Issue, attachments attachmentTransferables: [AttachmentTransferable]? = nil, audioAttachmentURL audioURL: URL? = nil) throws {
-        logger.debug("Adding comment")
+    /// Adds a new comment to core data.
+    /// - Parameters:
+    ///   - comment: The comment string to add.
+    ///   - issue: The issue to add the comment to.
+    ///   - attachmentTransferables: The image attachment(s) of the comment.
+    ///   - audioURL: The audio attachment of the comment.
+    func addComment(
+        comment: String,
+        to issue: Issue,
+        attachments attachmentTransferables: [AttachmentTransferable]? = nil,
+        audioAttachmentURL audioURL: URL? = nil
+    ) throws {
         let comment = Comment(comment: comment, context: viewContext)
+        logger.debug("Adding comment with id: \(comment.wrappedId)")
         
         var attachments = [Attachment]()
         var records = [CKRecord]()
@@ -160,6 +191,10 @@ extension PersistenceController {
         }
     }
     
+    /// Adds a new project to core data.
+    /// - Parameters:
+    ///   - name: The name of the project.
+    ///   - dateStarted: The start date for the project.
     func addProject(name: String, dateStarted: Date) throws {
         let project = Project(name: name, startDate: dateStarted, context: viewContext)
         logger.debug("Adding new project with id: \(project.wrappedId)")
@@ -167,12 +202,19 @@ extension PersistenceController {
         
     }
     
+    /// Deletes the given object from core data.
+    /// - Parameter object: The object to delete.
     func deleteObject<T: NSManagedObject>(_ object: T) throws {
         viewContext.delete(object)
         logger.debug("Deleting object with id: \(object.objectID)")
         try save()
     }
     
+    /// Adds an attachment to a comment.
+    /// - Parameters:
+    ///   - attachmentType: The type of the attachment (Image or audio).
+    ///   - attachmentURL: The URL for the attachment.
+    ///   - comment: The comment to add the attachment to.
     func addAttachment(ofType attachmentType: AttachmentType, attachmentURL: URL, to comment: Comment) throws {
         let attachment = Attachment(context: viewContext)
         attachment.assetURL = attachmentURL
@@ -186,6 +228,7 @@ extension PersistenceController {
         try save()
     }
     
+    /// Commits the changes made to core data.
     func save() throws {
         if !viewContext.hasChanges {
             logger.debug("Failed to save. managed object has no changes.")
@@ -197,6 +240,7 @@ extension PersistenceController {
 }
 
 extension PersistenceController {
+    /// A preview `PersistenceController` for the ui previews.
     static var preview: PersistenceController = {
         let controller = PersistenceController(inMemory: true)
         let viewContext = controller.container.viewContext
