33994947fcea75b7eb9c205e8379a30bfd37b793
Tino Tusaumwe <tusatino@gmail.com>
2023-04-28 19:52:37 +1000

    Move RadioToggleStyle to its own file

--- IssueTracker/Views/Components/IssueRowView.swift
+++ IssueTracker/Views/Components/IssueRowView.swift
@@ -7,25 +7,6 @@
 
 import SwiftUI
 
-struct RadioToggleStyle: ToggleStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        Button {
-            configuration.isOn.toggle()
-        } label: {
-            Image(systemName: configuration.isOn ? SFSymbol.largeCircleFillCircle : SFSymbol.circle)
-                .font(.title)
-                .animation(.default, value: configuration.isOn)
-        }
-        .buttonStyle(.plain)
-    }
-}
-
-extension ToggleStyle where Self == RadioToggleStyle {
-    static var radioButton: RadioToggleStyle {
-        RadioToggleStyle()
-    }
-}
-
 struct IssueRowView: View {
     var issueProperties: IssueProperties
     @State private var isOpen = false
